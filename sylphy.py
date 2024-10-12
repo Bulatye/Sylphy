5336c7a7bca3b98ebf0401e8f36295c395ed707f
bulatmaratovich <bulatgroyper@gmail.com>
2024-10-13 01:02:48 +0300

    Added keys that return only the original lyrics of the song or its translation

--- sylphy.py
+++ sylphy.py
@@ -6,38 +6,45 @@ from bs4 import BeautifulSoup
 import re
     
 def main():
-    # Parse arguments
     parser = argparse.ArgumentParser()
+    # Parse arguments
     parser.add_argument('artist', type=str)
     parser.add_argument('song', type=str)
-
+    parser.add_argument('-ot','--origtrans', action='store', help='Get the original text and its translation')
+    parser.add_argument('-o', '--original', action='store_true', help='Get only original text')
+    parser.add_argument('-t', '--translate', action='store_true', help='Get only translate text')
     args = parser.parse_args()
-    
-    artist = args.artist.lower().strip()
-    song = args.song.lower().strip()
 
-    artist = re.sub(r'\s+', '_', artist)
-    song = re.sub(r'\s+', '_', song)
-    first_char = artist[0]
+    # processing user input, replacing spaces in words with underscores
+    artist = re.sub(r'\s+', '_', args.artist).lower().strip()
+    song = re.sub(r'\s+', '_', args.song).lower().strip()
 
-
-    url = f'https://www.amalgama-lab.com/songs/{first_char}/{artist}/{song}.html'
+    # create url
+    url = f'https://www.amalgama-lab.com/songs/{artist[0]}/{artist}/{song}.html'
+    # I get html with data about the song
     html_content = requests.get(url).text
 
+    # Get the original text and translation
     soup = BeautifulSoup(html_content, 'html.parser')
     original_text = soup.find_all(class_='original')
     translate_text = soup.find_all(class_='translate')
     
+    # Print text in terminal
     print()
     for i in range(1, len(original_text)):
-        if "перевод" in translate_text[i].text or "1 -" in translate_text[i].text:
+        if 'перевод' in translate_text[i].text or '1 -' in translate_text[i].text:
             break
-        elif original_text[i].text == "" or translate_text[i].text == "":
+        elif original_text[i].text == '' or translate_text[i].text == '':
             print()
         else:
-            print(original_text[i].text + " — " + translate_text[i].text)
+            if args.original:
+                print(original_text[i].text)
+            elif args.translate:
+                print(translate_text[i].text)
+            else:
+                print(original_text[i].text + ' — ' + translate_text[i].text)
     
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
 
